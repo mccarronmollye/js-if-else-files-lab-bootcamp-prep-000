8299cf812a0c6b3c2528e7f15b459d906451dd6e
mccarronmollye <mccarronmollye@gmail.com>
2018-09-19 11:33:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var name = "Joe"
 var height = 74
 
 if (name === "Joe") {
+  console.log("Expected: Susan, Received: Joe")
+} else {
   console.log("The name is correct")
 }
 
 if (height === 74) {
+  console.log("Expected: 70, Received: 74")
+} else {
   console.log("The height is correct")
 }
 // Don't worry about this
